ef393f12ab0f1546a22013206f84ff4faf3a9551
Darielly Santos <daryh_cristina@hotmail.com>
2021-11-21 19:26:38 -0300

    feat: configured the use of environment variable with .env file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
-app.listen(3535, () => {
-  console.log("port 3535 on, be glad!");
+const serverPort = process.env.APP_PORT || 3000;
+
+app.listen(serverPort, () => {
+  console.log("port ", serverPort, " on, be glad!");
 });
