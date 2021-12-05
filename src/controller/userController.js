517acd2272b41f18ff8f1928deaf68d0d57a0ca8
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-05 09:09:57 -0300

    fix: remove controller route get all user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,17 +1,6 @@
 const mongoose = require("mongoose");
 const UserSchema = require("../models/usersSchema");
 
-const getAll = async (req, res) => {
-  try {
-    const users = await UserSchema.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
-
 const getById = async (req, res) => {
   try {
     const user = req.params.id;
