752b3a40d405b767128af564da0e4b832b88d6e7
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-05 08:50:10 -0300

    fix: chanded route user

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/acheiumpet", userRoutes);
+app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 db.connect();
