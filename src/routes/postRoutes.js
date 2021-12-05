ac501de910bb396391cccb8003cf937ee2159d24
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-05 08:10:33 -0300

    fix: changed route delete post

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,7 +5,7 @@ const postController = require("../controller/postController");
 
 postRouter.post("/", postController.createPost);
 postRouter.get("/user/:id/posts", postController.getByUserId);
-postRouter.delete("/user/post/:id", postController.deletePostById);
+postRouter.delete("/post/:id", postController.deletePostById);
 postRouter.get("/", postController.getAll);
 postRouter.get("/:id", postController.getById);
 postRouter.post("/:id/up", postController.createUp);
