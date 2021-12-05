db2eee0c149db885dfa16d80c2dbcfe82f8ffb10
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-05 16:37:57 -0300

    feat: add jwt token returned into login endpoint response

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,11 +1,14 @@
 const UserSchema = require("../models/usersSchema");
+var jwt = require("jsonwebtoken");
 
 const createLogin = async (req, res) => {
   const body = req.body;
   const user = await UserSchema.find({ email: body.email });
 
   if (body.password == user[0].password) {
-    res.status(200).json("ok");
+    var token = jwt.sign({ userId: user._id }, "secretKey");
+
+    res.status(200).json(token);
   }
   res.status(401).json("invalid");
 };
