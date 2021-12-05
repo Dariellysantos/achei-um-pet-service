10d636d054a1864fe1ef16152a5797df748f79d3
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-05 08:11:23 -0300

    fix: chanded route get by user id

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,7 +4,7 @@ const postRouter = express.Router();
 const postController = require("../controller/postController");
 
 postRouter.post("/", postController.createPost);
-postRouter.get("/user/:id/posts", postController.getByUserId);
+postRouter.get("/user/:id", postController.getByUserId);
 postRouter.delete("/post/:id", postController.deletePostById);
 postRouter.get("/", postController.getAll);
 postRouter.get("/:id", postController.getById);
