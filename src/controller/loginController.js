4e86eb20873e63a87d96ae01d90b151d843863fd
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-08 22:55:08 -0300

    feat:  created verification for email does not exist

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -5,6 +5,14 @@ const createLogin = async (req, res) => {
   const body = req.body;
   const user = await UserSchema.find({ email: body.email });
 
+  if (user.length === 0) {
+    res.status(404).json({
+      message: "E-mail não encontrada",
+      code: "NOT_FOUND_ERROR",
+      data: null,
+    });
+  }
+
   if (body.password == user[0].password) {
     var token = jwt.sign({ userId: user[0]._id }, process.env.JWT_SECRET_KEY);
 
