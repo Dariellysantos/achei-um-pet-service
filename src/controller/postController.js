4d5d183990cd7847ebd0d33f36db1895486cfe4e
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-11 18:13:55 -0300

    fix: test error token

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -170,7 +170,10 @@ const getById = async (req, res) => {
   const authHeader = req.get("authorization");
 
   if (!authHeader) {
-    return res.status(401).send("erro no header");
+    return res.status(401).json({
+      message: "It did not receive a token jwt",
+      code: "NOT_AUTHORIZED_WITHOUT_TOKEN",
+    });
   }
   try {
     const postId = req.params.id;
