4b187ea4480c6e2e64d6536bff60b60db5098ae4
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-12 13:53:39 -0300

    fix: config procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const serverPort = process.env.APP_PORT || 3000;
+const serverPort = process.env.PORT || 3000;
 
 app.listen(serverPort, () => {
   console.log("port ", serverPort, " on, be glad!");
