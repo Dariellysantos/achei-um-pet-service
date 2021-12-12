36308d3f9d70682eee8805f52ff977002ebafe7d
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-12 00:27:09 -0300

    fix: change mongodb URI format

--- src/database/mongoConfig.js
+++ src/database/mongoConfig.js
@@ -1,19 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI =
-  "mongodb+srv://" +
-  process.env.DB_USER +
-  ":" +
-  process.env.DB_PASSWORD +
-  "@" +
-  process.env.DB_HOST +
-  "/" +
-  process.env.DB_NAME +
-  "?retryWrites=true&w=majority";
-
 const connect = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
