4614e0610b50e9a4c502da7add6b9573b93a7ea1
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-12 00:16:21 -0300

    test: add createLogin unit test when success

---
+++ src/controller/loginController .test.js
@@ -0,0 +1,47 @@
+const UserSchema = require("../models/usersSchema");
+const loginController = require("../controller/loginController");
+const mockingoose = require("mockingoose");
+const jwt = require("jsonwebtoken");
+
+describe("getLogin unit test", () => {
+  test("success", async () => {
+    mockingoose(UserSchema).toReturn(
+      [{ email: "test@email.com", password: "123456" }],
+      "find"
+    );
+
+    jest
+      .spyOn(jwt, "sign")
+      .mockReturnValue(
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTlmNzExMzNlYzQ3ZTIwMTUwNzczOGMiLCJpYXQiOjE2Mzg3NTA4NDN9.e13u83p6cg6Quk8gk4dZ5pulc7uLhISqdAEF-wplyLE"
+      );
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn();
+
+    await loginController.createLogin(
+      {
+        body: {
+          email: "test@email.com",
+          password: "123456",
+        },
+      },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Acesso liberado",
+      code: "SUCCESS",
+      data: {
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTlmNzExMzNlYzQ3ZTIwMTUwNzczOGMiLCJpYXQiOjE2Mzg3NTA4NDN9.e13u83p6cg6Quk8gk4dZ5pulc7uLhISqdAEF-wplyLE",
+      },
+    });
+  });
+  test("fail and return 404 when email does not exists", () => {});
+  test("fail and return 401 when password is incorrect", () => {});
+  test("fail and return 500 when password is incorrect", () => {});
+});
