e62602746796dfc9ec9d3bfb1ffa99f86fdf92bb
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-13 20:00:47 -0300

    fix: changed text success

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -69,7 +69,8 @@ const createPost = async (req, res) => {
 
     return res.status(200).json({
       message: "Post registered successfully!",
-      sevedPost,
+      code: "SUCCESS",
+      data: sevedPost,
     });
   } catch (error) {
     return res.status(500).json({
