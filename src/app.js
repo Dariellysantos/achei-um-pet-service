84734effbc0c32e3fe3dcc229ee5bbb320824f87
Darielly Santos <daryh_cristina@hotmail.com>
2021-12-13 22:22:48 -0300

    fix: rename routers file name

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ const cors = require("cors");
 require("dotenv").config();
 const db = require("./database/mongoConfig");
 
-const userRoutes = require("./routes/userRoutes");
-const postRoutes = require("./routes/postRoutes");
+const userRouter = require("./routes/userRouter");
+const postRouter = require("./routes/postRouter");
 const loginRouter = require("./routes/loginRouter");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 app.use("/login", loginRouter);
 
 db.connect();
