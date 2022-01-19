d19ce4729dc0f8f5a4f684d48e323c63510ee350
Darielly Santos <daryh_cristina@hotmail.com>
2022-01-18 21:42:45 -0300

    fix: changed post name schema

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,8 +1,8 @@
-const schemaPost = require("../models/Schema/postsSchema");
+const schema = require("../models/Schema/postsSchema");
 const { convertPostDbToPost } = require("./converters/postsConverter");
 
 exports.getById = async (postId) => {
-  const post = await PostSchema.findById(postId);
+  const post = await schema.findById(postId);
 
   if (post.lenth === 0) {
     return [];
